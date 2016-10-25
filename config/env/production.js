6a76cf6e5df091407870caab6dceeef28b84e05d
Chintan Shah <chintan@wohlig.com>
2016-10-25 19:24:51 +0530

    proxy changes

--- config/env/production.js
+++ config/env/production.js
@@ -25,8 +25,8 @@ module.exports = {
    * Set the port in the production environment to 80                        *
    ***************************************************************************/
 
-  port: 1339,
-  realHost: "http://104.155.129.33:1339",
+  port: 80,
+  realHost: "http://bachlors.tingdom.in",
 
 
 
