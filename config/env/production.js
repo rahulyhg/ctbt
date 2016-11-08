3c252157b78451a0760f493b13f8c20ec6c44f88
deshmukhsanket <sanketdeshmukh7206@gmail.com>
2016-11-08 12:43:33 +0530

    Url production

--- config/env/production.js
+++ config/env/production.js
@@ -25,8 +25,8 @@ module.exports = {
    * Set the port in the production environment to 80                        *
    ***************************************************************************/
 
-  port: 80,
-  realHost: "http://wohlig.io",
+  port: 1339,
+  realHost: "http://bachlors.tingdom.in",
 
 
 
