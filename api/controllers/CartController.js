7e6a572fa6367e542d5f47cc13d0203bdf90fabc
deshmukhsanket <sanketdeshmukh7206@gmail.com>
2016-11-10 12:11:15 +0530

    Delete All Cart

--- api/controllers/CartController.js
+++ api/controllers/CartController.js
@@ -190,6 +190,21 @@ var controller = {
       });
     }
   },
-
+ deleteAllCart: function(req, res) {
+       req.session.destroy(function(err) {
+           if (err) {
+               res.json({
+                   value: false,
+                   error: err
+               });
+           } else {
+               setTimeout(function() {
+                   res.json({
+                       value: true
+                   });
+               }, 3000);
+           }
+       });
+   },
 };
 module.exports = _.assign(module.exports, controller);
