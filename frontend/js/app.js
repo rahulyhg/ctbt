bfc98d4622ee3f679124f355667fa1d4ccfb2189
Chaitalee <chaitalee.wohlig@gmail.com>
2016-11-18 19:19:11 +0530

    changes

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,12 +1,12 @@
 // JavaScript Document
 var firstapp = angular.module('firstapp', [
-  'ui.router',
-  'phonecatControllers',
-  'templateservicemod',
-  'navigationservice',
-  'pascalprecht.translate',
-  'angulartics',
-  'angulartics.google.analytics'
+    'ui.router',
+    'phonecatControllers',
+    'templateservicemod',
+    'navigationservice',
+    'pascalprecht.translate',
+    'angulartics',
+    'angulartics.google.analytics'
 ]);
 firstapp.directive('fancybox', function($compile, $parse) {
     return {
@@ -35,85 +35,85 @@ firstapp.directive('fancybox', function($compile, $parse) {
     };
 });
 firstapp.config(function($stateProvider, $urlRouterProvider, $httpProvider, $locationProvider) {
-  // for http request with session
-  $httpProvider.defaults.withCredentials = true;
-  $stateProvider
-    .state('home', {
-      url: "/",
-      templateUrl: "frontend/views/template.html",
-      controller: 'HomeCtrl'
-    })
+    // for http request with session
+    $httpProvider.defaults.withCredentials = true;
     $stateProvider
-      .state('comingsoon', {
-        url: "/comingsoon",
-        templateUrl: "frontend/views/template.html",
-        controller: 'ComingsoonCtrl'
-      })
-  $stateProvider
-    .state('tbtstatic', {
-      url: "/tbtstatic",
-      templateUrl: "frontend/views/template.html",
-      controller: 'StaticCtrl'
-    })
-    .state('customisation', {
-      url: "/customisation/:id",
-      templateUrl: "frontend/views/template.html",
-      controller: 'CustomisationCtrl'
-    })
+        .state('home', {
+            url: "/",
+            templateUrl: "frontend/views/template.html",
+            controller: 'HomeCtrl'
+        })
+    $stateProvider
+        .state('comingsoon', {
+            url: "/comingsoon",
+            templateUrl: "frontend/views/template.html",
+            controller: 'ComingsoonCtrl'
+        })
+    $stateProvider
+        .state('tbtstatic', {
+            url: "/tbtstatic",
+            templateUrl: "frontend/views/template.html",
+            controller: 'StaticCtrl'
+        })
+        .state('customisation', {
+            url: "/customisation/:id",
+            templateUrl: "frontend/views/template.html",
+            controller: 'CustomisationCtrl'
+        })
 
-  .state('destination', {
-      url: "/destination",
-      templateUrl: "frontend/views/template.html",
-      controller: 'DestinationCtrl'
-    })
-    .state('pattaya', {
-      url: "/pattaya/:id",
-      templateUrl: "frontend/views/template.html",
-      controller: 'PattayaCtrl'
-    })
-    .state('pattaya2', {
-      url: "/pattaya2/:id",
-      templateUrl: "frontend/views/template.html",
-      controller: 'Pattaya2Ctrl'
-    })
-    .state('whats-hot-more', {
-      url: "/whats-hot-more/:id",
-      templateUrl: "frontend/views/template.html",
-      controller: 'Whats-hot-moreCtrl'
-    })
-    .state('whats-hot', {
-      url: "/whats-hot",
-      templateUrl: "frontend/views/template.html",
-      controller: 'WhatsHotCtrl'
-    })
-    .state('activity', {
-      url: "/activity",
-      templateUrl: "frontend/views/template.html",
-      controller: 'ActivityCtrl'
-    });
-  $urlRouterProvider.otherwise("/");
-  $locationProvider.html5Mode(isproduction);
+    .state('destination', {
+            url: "/destination",
+            templateUrl: "frontend/views/template.html",
+            controller: 'DestinationCtrl'
+        })
+        .state('pattaya', {
+            url: "/pattaya/:id",
+            templateUrl: "frontend/views/template.html",
+            controller: 'PattayaCtrl'
+        })
+        .state('pattaya2', {
+            url: "/pattaya2/:id",
+            templateUrl: "frontend/views/template.html",
+            controller: 'Pattaya2Ctrl'
+        })
+        .state('whats-hot-more', {
+            url: "/whats-hot-more/:id",
+            templateUrl: "frontend/views/template.html",
+            controller: 'Whats-hot-moreCtrl'
+        })
+        .state('whats-hot', {
+            url: "/whats-hot",
+            templateUrl: "frontend/views/template.html",
+            controller: 'WhatsHotCtrl'
+        })
+        .state('activity', {
+            url: "/activity",
+            templateUrl: "frontend/views/template.html",
+            controller: 'ActivityCtrl'
+        });
+    $urlRouterProvider.otherwise("/");
+    $locationProvider.html5Mode(isproduction);
 });
 
 
 firstapp.directive('img', function($compile, $parse) {
-  return {
-    restrict: 'E',
-    replace: false,
-    link: function($scope, element, attrs) {
-      var $element = $(element);
-      if (!attrs.noloading) {
-        $element.after("<img src='frontend/img/loading.gif' class='loading' />");
-        var $loading = $element.next(".loading");
-        $element.load(function() {
-          $loading.remove();
-          $(this).addClass("doneLoading");
-        });
-      } else {
-        $($element).addClass("doneLoading");
-      }
-    }
-  };
+    return {
+        restrict: 'E',
+        replace: false,
+        link: function($scope, element, attrs) {
+            var $element = $(element);
+            if (!attrs.noloading) {
+                $element.after("<img src='frontend/img/loading.gif' class='loading' />");
+                var $loading = $element.next(".loading");
+                $element.load(function() {
+                    $loading.remove();
+                    $(this).addClass("doneLoading");
+                });
+            } else {
+                $($element).addClass("doneLoading");
+            }
+        }
+    };
 });
 firstapp.directive('autoHeight', function($compile, $parse) {
     return {
@@ -162,56 +162,56 @@ firstapp.filter('serverimage', function() {
 });
 // :130:130:cover
 firstapp.directive('fancyboxBox', function($document) {
-  return {
-    restrict: 'EA',
-    replace: false,
-    link: function(scope, element, attr) {
-      var $element = $(element);
-      var target;
-      if (attr.rel) {
-        target = $("[rel='" + attr.rel + "']");
-      } else {
-        target = element;
-      }
+    return {
+        restrict: 'EA',
+        replace: false,
+        link: function(scope, element, attr) {
+            var $element = $(element);
+            var target;
+            if (attr.rel) {
+                target = $("[rel='" + attr.rel + "']");
+            } else {
+                target = element;
+            }
 
-      target.fancybox({
-        openEffect: 'fade',
-        closeEffect: 'fade',
-        closeBtn: true,
-        helpers: {
-          media: {}
+            target.fancybox({
+                openEffect: 'fade',
+                closeEffect: 'fade',
+                closeBtn: true,
+                helpers: {
+                    media: {}
+                }
+            });
         }
-      });
-    }
-  };
+    };
 });
 
 firstapp.config(function($translateProvider) {
-  $translateProvider.translations('en', LanguageEnglish);
-  $translateProvider.translations('hi', LanguageHindi);
-  $translateProvider.preferredLanguage('en');
+    $translateProvider.translations('en', LanguageEnglish);
+    $translateProvider.translations('hi', LanguageHindi);
+    $translateProvider.preferredLanguage('en');
 });
 firstapp.filter('youtubethumb', function() {
     return function(input, onlyid) {
         if (input) {
-          console.log(input);
+            console.log(input);
             return "http://img.youtube.com/vi/" + input + "/hqdefault.jpg";
             // console.log();
         }
     };
 });
 firstapp.filter('rawHtml', ['$sce',
-   function ($sce) {
-       return function (val) {
-           console.log(val);
-           return $sce.trustAsHtml(val);
-       };
-   }
+    function($sce) {
+        return function(val) {
+            console.log(val);
+            return $sce.trustAsHtml(val);
+        };
+    }
 ]);
-firstapp.filter('trusted', ['$sce', function ($sce) {
-   return function (url) {
-       return $sce.trustAsResourceUrl(url);
-   };
+firstapp.filter('trusted', ['$sce', function($sce) {
+    return function(url) {
+        return $sce.trustAsResourceUrl(url);
+    };
 }]);
 firstapp.filter('shorten', function() {
     return function(value, limit) {
@@ -229,7 +229,7 @@ firstapp.filter('shorten', function() {
 
 firstapp.filter('TbtIcon', function() {
     return function(input) {
-console.log(input);
+        console.log(input);
         var returnImg = "frontend/img/tbtIcons/";
         switch (input) {
             case "The City":
@@ -270,6 +270,18 @@ console.log(input);
 
             case "For Art enthusiasts":
                 returnImg += "for-art-enthusiasts.png";
+                break;
+
+            case "Things not to do here":
+                returnImg += "Things-not-to-do-here.png";
+                break;
+
+            case "Things to take back":
+                returnImg += "Things-to-take-back.png";
+                break;
+
+            case "Events and Festivals":
+                returnImg += "Festivals.png";
                 break;
 
         }
