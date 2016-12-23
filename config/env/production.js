6bb3bf552ae0d0caaf52fe1024cd9bfa7cd9a510
deshmukhsanket <sanketdeshmukh7206@gmail.com>
2016-12-23 17:04:02 +0530

    port change

--- config/env/production.js
+++ config/env/production.js
@@ -25,7 +25,7 @@ module.exports = {
    * Set the port in the production environment to 80                        *
    ***************************************************************************/
 
-  port: 1339,
+  port: 80,
   // realHost: "http://bachlors.tingdom.in",
    realHost: "http://thebachelortrip.tingdom.in",
 //1339
