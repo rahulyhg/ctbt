d3f11148170da18ea6c7c5637edf22bedc987a99
pratikg17 <pratikg17@github.com>
2017-01-30 15:51:57 +0530

    passport js

--- config/passport/google.js
+++ config/passport/google.js
@@ -2,13 +2,13 @@
 // global["GoogleKey"] = "AIzaSyBzblvTfnRrpg33LBDzqiMXPqY7dmpwaCk";
  // global["GoogleclientSecret"] = "M4zqmRFKLugMxRZcpXDmj7jZ"; // OG
 
-//  global["GoogleKey"] = "AIzaSyBzblvTfnRrpg33LBDzqiMXPqY7dmpwaCk";
-//  global["GoogleclientId"] = "1062269407794-i114uc8qupien017gd1aemd5bc88i618.apps.googleusercontent.com";
-//  global["GoogleclientSecret"] = "M4zqmRFKLugMxRZcpXDmj7jZ";
-
  global["GoogleKey"] = "AIzaSyBzblvTfnRrpg33LBDzqiMXPqY7dmpwaCk";
- global["GoogleclientId"] = "1062269407794-imvp331g9e4dfets6sgkp8sn0ardccim.apps.googleusercontent.com";
- global["GoogleclientSecret"] = "jjrWoJ5dKU4iAUgk1kvgsZIX";
+ global["GoogleclientId"] = "1062269407794-i114uc8qupien017gd1aemd5bc88i618.apps.googleusercontent.com";
+ global["GoogleclientSecret"] = "M4zqmRFKLugMxRZcpXDmj7jZ";
+
+//  global["GoogleKey"] = "AIzaSyBzblvTfnRrpg33LBDzqiMXPqY7dmpwaCk";
+//  global["GoogleclientId"] = "1062269407794-imvp331g9e4dfets6sgkp8sn0ardccim.apps.googleusercontent.com";
+//  global["GoogleclientSecret"] = "jjrWoJ5dKU4iAUgk1kvgsZIX";
 passport.use(new GoogleStrategy({
         clientId: GoogleclientId,
         clientSecret: GoogleclientSecret,
